9a00869b71da849d7dd1be92c06556ce4a5fae37
hwang434 <inqbator@knou.ac.kr>
2021-06-21 23:03:58 +0900

    [LeetCode] 1859.Sorting the Sentence
    버블 정렬을 활용한 알고리즘

---
+++ LeetCode/Sort/1859. Sorting the Sentence/IngyuBubbleSort.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void swap(String[] sArray, int i, int j) {
+        String temp = sArray[i];
+        sArray[i] = sArray[j];
+        sArray[j] = temp;
+    }
+
+    public String sortSentence(String s) {
+        String[] sArray = s.split(" ");
+
+        for(int i = 1; i < sArray.length; i++) {
+            for(int j = 0; j < sArray.length - i; j++) {
+                if(sArray[j].charAt(sArray[j].length()-1) > sArray [j + 1].charAt(sArray[j+1].length()-1)) {
+                    swap(sArray, j, j + 1);
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String string : sArray) {
+            sb.append(string.substring(0, string.length()-1));
+            sb.append(" ");
+        }
+        sb.deleteCharAt(sb.length()-1);
+
+        return sb.toString();
+    }
+}
