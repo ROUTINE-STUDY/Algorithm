5dc2399b729434f3ebda142c958d3abc3199e31f
hwang <inqbator@knou.ac.kr>
2021-07-03 00:27:07 +0900

    [LeetCode] 169. Majority Element 잘못 올린 파일 수정.
    수정했습니당.

--- LeetCode/Array/169. Majority Element/IngyuMap.java
+++ LeetCode/Array/169. Majority Element/IngyuMap.java
@@ -1,33 +1,23 @@
-class Solution1 {
-    public String reverseWords(String s) {
-        char[] reversedString = new char[s.length()];
+import java.util.HashMap;
+import java.util.Map;
 
-        int start = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int end = 0;
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int answer = nums[0];
+        int maxNumCount = 0;
 
-            if (s.charAt(i) == ' ') {
-                end = i-1;
-                swap(start,end,s,reversedString);
-                reversedString[i] = ' ';
-                start = i+1;
-            } else if (i == s.length()-1) {
-                end = i;
-                swap(start,end,s,reversedString);
-                start = i+1;
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+
+            if (i >= nums.length/2) {
+                if (map.get(nums[i]) > maxNumCount) {
+                    maxNumCount = map.get(nums[i]);
+                    answer = nums[i];
+                }
             }
         }
 
-        return new String(reversedString);
-    }
-
-    private void swap(int start, int end,String original, char[] reversedString) {
-        while (start <= end) {
-            reversedString[start] = original.charAt(end);
-            reversedString[end] = original.charAt(start);
-
-            start++;
-            end--;
-        }
+        return answer;
     }
 }
