78cdac3afeee5833607f0581ac35711b2eaeb248
hwang <inqbator@knou.ac.kr>
2021-07-17 16:10:47 +0900

    Update IngyuBinary.java

--- LeetCode/Dynamic Programming/338. Counting Bits/IngyuBinary.java
+++ LeetCode/Dynamic Programming/338. Counting Bits/IngyuBinary.java
@@ -1,30 +1,19 @@
-import java.util.Arrays;
+class Solution {
+    public int[] countBits(int n) {
+        int[] answer = new int[n+1];
 
+        for (int i = 0; i <= n; i++) {
+            int temp = i;
 
+            int numberOfOne = 0;
+            do {
+                numberOfOne += temp % 2;
+                temp /= 2;
+            } while (temp > 0);
 
-public class IngyuBinary {
-    static class Solution {
-        public int[] countBits(int n) {
-            int[] answer = new int[n+1];
-
-            for (int i = 0; i <= n; i++) {
-                int temp = i;
-
-                int numberOfOne = 0;
-                do {
-                    numberOfOne += temp % 2;
-                    temp /= 2;
-                } while (temp > 0);
-
-                answer[i] = numberOfOne;
-            }
-
-            return answer;
+            answer[i] = numberOfOne;
         }
-    }
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        System.out.println("ss");
-        s.countBits(5);
+
+        return answer;
     }
 }
