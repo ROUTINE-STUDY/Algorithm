5e51c7b0843b10fd14ad0fae7b9ac11991be7aac
kaki <ckdbioksh@naver.com>
2021-08-03 15:15:21 +0900

    [LeetCode] 1. Two Sum

---
+++ LeetCode/Array/1. Two Sum/kaki5507.java
@@ -0,0 +1,30 @@
+package leetcodeLv2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class twosum {
+
+	public static void main(String[] args) {
+		int nums[] = {2,7,11,15};
+		int target = 9;
+		
+		// 일단, 정답으로 만들어 줄 Array를 만들어준다.
+		// 유효한 대답은 하나만 존재한다고 한다.
+		/*
+        for(int i=0; i<nums.length; i++) {
+			for(int j=0; j<nums.length; j++) {
+				if(nums[i] + nums[j] == target && i != j) {
+					int[] dab = {i,j};
+					return dab;
+				}
+			}
+		}
+        return nums;
+		*/
+		
+		
+	}
+
+}
