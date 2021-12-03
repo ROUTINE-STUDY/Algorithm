e4f87766e75f7b4446b0d34598a525791ea52b98
kaki5507 <ckdbioksh@naver.com>
2021-12-03 23:02:31 +0900

    [프로그래머스] 수박수박수박수박수박수

---
+++ Programmers/연습문제/수박수박수박수박수박수/kaki5507.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        
+        for(int i=0; i<n; i++){
+            if(i % 2 == 0){
+                answer+="수";
+            }else{
+                answer+="박";
+            }
+        }
+        
+        return answer;
+    }
+}
