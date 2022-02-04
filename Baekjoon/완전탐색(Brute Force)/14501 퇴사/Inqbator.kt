d82bf775fd8470f6d31271cc231f86b6acf4a569
hwang434 <inqbator@knou.ac.kr>
2022-02-04 23:06:47 +0900

    동적 프로그래밍 추가

--- Baekjoon/완전탐색(Brute Force)/14501 퇴사/Inqbator.kt
+++ Baekjoon/완전탐색(Brute Force)/14501 퇴사/Inqbator.kt
@@ -1,31 +1,36 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
-// 1. 완전탐색
-// 2. DP
-var sumOfCost = 0
+
+
+var N = 0
+var answer = 0
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    val remainingDay = readLine().toInt()
-    val arrOfTheDaysTake = IntArray(remainingDay)
-    val arrOfCosts = IntArray(remainingDay)
-    for (day in 0 until remainingDay) {
+    N = readLine().toInt()
+    val arrOfTheDaysTake = IntArray(N)
+    val arrOfCosts = IntArray(N)
+    for (day in  0 until N) {
         val (period, cost) = readLine().split(" ").map { it.toInt() }
         arrOfTheDaysTake[day] = period
         arrOfCosts[day] = cost
     }
 
-    getMaximumCost(arrOfTheDaysTake, arrOfCosts, 0, 0)
-    println(sumOfCost)
+    val dp = IntArray(arrOfTheDaysTake.size+1)
+    getMaximumCost(arrOfTheDaysTake, arrOfCosts, 0, dp)
+    println(answer)
 }
 
-fun getMaximumCost(arrOfTheDaysTake: IntArray, arrOfCosts: IntArray, currentDay: Int, currentCost: Int) {
-    if (currentDay >= arrOfTheDaysTake.size) {
-        sumOfCost = sumOfCost.coerceAtLeast(currentCost)
+fun getMaximumCost(arrOfTheDaysTake: IntArray, arrOfCosts: IntArray, currentDay: Int, dp: IntArray) {
+    if (currentDay >= N) {
         return
+    } else if (currentDay > 0) {
+        dp[currentDay] = dp[currentDay].coerceAtLeast(dp[currentDay-1])
     }
 
     val nextDay = currentDay + arrOfTheDaysTake[currentDay]
-    if (nextDay <= arrOfTheDaysTake.size) {
-        getMaximumCost(arrOfTheDaysTake, arrOfCosts, nextDay, currentCost+arrOfCosts[currentDay])
+    if (nextDay < N+1) {
+        dp[nextDay] = dp[nextDay].coerceAtLeast(dp[currentDay]+arrOfCosts[currentDay])
+        answer = answer.coerceAtLeast(dp[nextDay])
     }
-    getMaximumCost(arrOfTheDaysTake, arrOfCosts, currentDay+1, currentCost)
+
+    getMaximumCost(arrOfTheDaysTake, arrOfCosts, currentDay+1, dp)
 }
