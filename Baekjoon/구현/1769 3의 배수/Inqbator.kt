aac1de781da327872492a095e170e3086057152c
hwang434 <inqbator@knou.ac.kr>
2022-03-25 22:51:12 +0900

    [백준] 3의 배수

---
+++ Baekjoon/구현/1769 3의 배수/Inqbator.kt
@@ -0,0 +1,35 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+var cnt = 0
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    var X = readLine()
+    val outputNum = getCntOfLoop(X)
+
+    println(cnt)
+    if (outputNum % 3 == 0) println("YES")
+    else println("NO")
+}
+
+fun getCntOfLoop(X: String): Int {
+    if (X.length < 2) {
+        return X.toInt()
+    }
+
+    var x = StringBuilder(X)
+    var temp = 0
+    while (true) {
+        cnt++
+        for (i in x.length-1 downTo 0) {
+            temp += x[i].digitToInt()
+        }
+
+        if (temp < 10) {
+            break
+        }
+        x = StringBuilder(temp.toString())
+        temp = 0
+    }
+
+    return temp
+}
