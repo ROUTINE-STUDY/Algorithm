e5c54b8262a8b3905b27c0bb0fdc1a53bfdf95c4
hwang434 <inqbator@knou.ac.kr>
2022-11-14 22:29:18 +0900

    [프로그래머스] 양궁대회 리팩토링

--- Programmers/2022 KAKAO BLIND RECRUITMENT/양궁대회/Hwang434.kt
+++ Programmers/2022 KAKAO BLIND RECRUITMENT/양궁대회/Hwang434.kt
@@ -3,7 +3,7 @@ class Solution {
     var scoreDiffrence = 0
 
     fun solution(n: Int, info: IntArray): IntArray {
-        recursion(info, IntArray(11), 0, n)
+        recursion(info, IntArray(11), 10, n)
 
         return if (scoreDiffrence == 0) {
             intArrayOf(-1)
@@ -19,7 +19,7 @@ class Solution {
     }
 
     private fun recursion(apacheShotInfo: IntArray, lionShotInfo: IntArray, idx: Int, remainArrowCnt: Int) {
-        if (idx >= 11 || remainArrowCnt == 0) {
+        if (idx < 0 || remainArrowCnt == 0) {
             var currentApacheScore = 0
             var currentLionScore = 0
             for (i in 0..10) {
@@ -40,28 +40,16 @@ class Solution {
                 for (i in 0..10) {
                     answer[i] = lionShotInfo[i]
                 }
-            } else if (scoreDiffrence == currentLionScore - currentApacheScore) {
-                for (i in 10 downTo 0) {
-                    if (answer[i] > lionShotInfo[i]) {
-                        return
-                    } else if (answer[i] < lionShotInfo[i]) {
-                        scoreDiffrence = currentLionScore - currentApacheScore
-                        for (i in 0..10) {
-                            answer[i] = lionShotInfo[i]
-                        }
-                    }
-                }
             }
             return
         }
 
-        recursion(apacheShotInfo, lionShotInfo, idx + 1, remainArrowCnt)
+
         if (remainArrowCnt > apacheShotInfo[idx]) {
-            if (remainArrowCnt > apacheShotInfo[idx]) {
-                lionShotInfo[idx] = apacheShotInfo[idx] + 1
-                recursion(apacheShotInfo = apacheShotInfo, lionShotInfo = lionShotInfo, idx + 1, remainArrowCnt - apacheShotInfo[idx] - 1)
-            }
+            lionShotInfo[idx] = apacheShotInfo[idx] + 1
+            recursion(apacheShotInfo = apacheShotInfo, lionShotInfo = lionShotInfo, idx - 1, remainArrowCnt - apacheShotInfo[idx] - 1)
         }
         lionShotInfo[idx] = 0
+        recursion(apacheShotInfo, lionShotInfo, idx - 1, remainArrowCnt)
     }
 }
